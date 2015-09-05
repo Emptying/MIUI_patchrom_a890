c8c14a397c8cf27557ec0d86cd074b1407a42509
empty <cws1073031455@gmail.com>
2015-09-06 01:17:54 +0800

    add a890_miuiv6_device

---
+++ releasetools.py
@@ -0,0 +1,26 @@
+import common
+import edify_generator
+import os
+def RemoveDeviceAssert(info):
+  edify = info.script
+  for i in xrange(len(edify.script)):
+    if "ro.product" in edify.script[i]:
+      edify.script[i] = ''
+      return
+
+def WritePolicyConfig(info):
+  try:
+    file_contexts = info.input_zip.read("META/file_contexts")
+    common.ZipWriteStr(info.output_zip, "file_contexts", file_contexts)
+  except KeyError:
+    print "warning: file_context missing from target;"
+
+
+def FullOTA_InstallEnd(info):
+    RemoveDeviceAssert(info)
+    
+def IncrementalOTA_InstallEnd(info):
+    RemoveDeviceAssert(info)
+
+
+
